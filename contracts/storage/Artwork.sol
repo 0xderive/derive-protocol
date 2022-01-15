fc9eef758eea3678df99b8744d75e391ed3c76a8
troels_a <troels@blck.dk>
2022-01-15 18:08:50 +0100

    basic artwork contract in place

--- contracts/storage/Artwork.sol
+++ contracts/storage/Artwork.sol
@@ -2,23 +2,52 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./../utils/base64.sol";
 
 
-contract Artwork is Ownable {
 
-    mapping(uint => string) private _artworks;
-    
-    function getArtwork(uint coll_id_, bool base64_) public view returns (string memory) {
-    
-      string memory artwork_ = _artworks[coll_id_];
+////////////////////////////////////
+//                                //
+//           ARTWORK v1           //
+//                                //
+////////////////////////////////////
 
-      return artwork_;
 
-    }
 
-    function setArtwork(uint coll_id_, string memory artwork_) public returns (string memory){
-      require(bytes(_artworks[coll_id_]).length < 1, 'Artwork already set');
-      
-    }
+/**
+@dev Default artwork contract for editions
+*/
+
+contract Artwork_v1 is Ownable {
+
+  /// @dev artwork mapping. (edition id => artwork string)
+  mapping(uint => string) private _artworks;
+
+  /// @dev Get the artwork string for a given release
+  /// @param edition_id_ The id of the edition in the main contract
+  /// @param base64_ Boolean determining wether to base64 encode the output string
+  /// @return String containing the artwork url
+  function getArtwork(
+    uint edition_id_,
+    bool base64_
+  )
+  public view returns (string memory) {
+    string memory artwork_ = _artworks[edition_id_];
+    if(base64_)
+      artwork_ = Base64.encode(bytes(artwork_));
+    return artwork_;
+  }
+
+  /// @dev Allows the caller to set the artwork string for a given edition. Only settable once.
+  /// @param edition_id_ the edition to update
+  /// @param artwork_ A string with the artwork
+  function setArtwork(
+    uint edition_id_,
+    string memory artwork_
+  )
+  public onlyOwner {
+    require(bytes(_artworks[edition_id_]).length < 1, 'Artwork already set');
+    _artworks[edition_id_] = artwork_;
+  }
 
 }
--- contracts/storage/IArtwork.sol
+++ contracts/storage/IArtwork.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.2;
 
 interface IArtwork {
 
-    function getArtwork(uint edition_id_, bool base64_) external view returns(string memory);
-    
+  function getArtwork(uint edition_id_, bool base64_) external view returns(string memory);
+  function setArtwork(uint edition_id_, string memory artwork_) external;
+
 }
